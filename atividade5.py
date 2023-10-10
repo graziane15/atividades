a9237ee6fa89cecef6c84e7478cc2a9709ecab6d
graziane15 <maria10052008graziane@gmail.com>
2023-10-10 11:06:31 -0300

    grazi e mayra

--- atividade5.py
+++ atividade5.py
@@ -1,4 +1,3 @@
 d=float(input("quanto voce tem na carteira"))
-v=float(input("quanto esta o dolar"))
-r=d/v
-print("voce pode comprar US$",r)
+r=d/5.15
+print("voce pode comprar{}".format(r))
---
+++ desafio.py
@@ -0,0 +1,6 @@
+a=float(input('digite a altura da parede'))
+l=float(input('digite a largura da parede'))
+area=a*l
+print('a area da sua parede é ',area,'m²')
+tinta=area/2
+print('voce precisara de ',tinta,'latas de tinta')
